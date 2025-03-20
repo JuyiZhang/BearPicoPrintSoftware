db0ef4c5bbcda74f10670c7eab738de6f5eda260
Juyi Zhang <100589897+JuyiZhang@users.noreply.github.com>
2025-03-20 14:01:47 -0700

    update calibration algorithm

--- numerical/util.py
+++ numerical/util.py
@@ -8,6 +8,8 @@ def find_fpga_file(directory, ext=".lvbitx"):
     """
     search_directory: list = os.listdir(directory)
     for filename in search_directory:
+        if (filename == "venv"):
+            continue
         current_dir = os.path.join(directory, filename)
         if os.path.isdir(current_dir):
             for discover_dir in os.listdir(current_dir):
@@ -18,14 +20,16 @@ def find_fpga_file(directory, ext=".lvbitx"):
     return "Not Found"
 
 class printer:
-    def __init__(self, H, d, K=0.8333, Uz=3.5):
+    def __init__(self, H, d, K=0.8333, Uz=3.5, address="172.22.11.2"):
         self.H = H/1000 # mm to m
         self.d = d/1000 # mm to m
         self.K = K
         self.Uz = Uz*1000 # kV to V
         self.command = []
-        self.bitfile = ""
-        self.session = None
+        self.address = address
+        self.connected = False
+        self.session: nifpga.Session = None
+        self.calibrationData = []
         self.bindToNiFPGA()
         
     def updateValues(self, H, d, K=0.8333, Uz=3.5):
@@ -34,18 +38,49 @@ class printer:
         self.K = K
         self.Uz = Uz
         
+    def saveCalibrationData(self, coordinates: list[str], calibrationVoltages: list[list[float]]):
+        print(f"Saving calibration data")
+        for i in range(0,len(coordinates)):
+            coordinate_ux = float(calibrationVoltages[i][0])
+            coordinate_uy = float(calibrationVoltages[i][1])
+            coordinate_x = float(coordinates[i].split(",")[0])
+            coordinate_y = float(coordinates[i].split(",")[1])
+            self.calibrationData.append([coordinate_x, coordinate_y, coordinate_ux, coordinate_uy])
+        print(f"Calibration data saved")
+        print(self.calibrationData)
+        
     def bindToNiFPGA(self):
         # Search the FPGA .lvbitx file
-        self.bitfile = find_fpga_file("./", ".lvbitx")
-        with nifpga.Session(self.bitfile, "rio://172.22.11.2/RIO0") as session:
-            session.reset()
-            session.run()
-            my_control = session.registers['MyConstant']
-            my_register = session.registers['MyRegister']
-            my_control.write(3)
-            data = my_register.read()
-            print(data)  
+        bitfile = find_fpga_file("./", ".lvbitx")
+        try:
+            self.session = nifpga.Session(bitfile, f"rio://{self.address}/RIO0")
+        except Exception as e:
+            print(f"Error: {e}")
+            return -1
+        return 0
+            
+    
+    def verifySession(self):
+        self.session.reset()
+        self.session.run()
+        my_control = self.session.registers['MyConstant']
+        my_register = self.session.registers['MyRegister']
+        my_control.write(3)
+        data = my_register.read()
+        if data == 9:
+            self.connected = True
+            print("Connected to FPGA")
+            return 0
+        else:
+            self.connected = False
+            print("Failed to connect to FPGA")
+            return -1
         
+    
+    def updateAddress(self, address):
+        self.address = address
+        self.closeSession()
+        self.bindToNiFPGA()
     
     def clearCommand(self):
         self.command = []
@@ -75,8 +110,10 @@ class printer:
     
     def toU(self, x, y):
         print(x, y)
+        
         Ux = x*self.d/(self.K*self.H**2)*self.Uz
         Uy = y*self.d/(self.K*self.H**2)*self.Uz
+        
         # First calculation, assume Ua + Uc = Ub + Ud
         Ua = (1250 + Ux)/2
         Uc = (1250 - Ux)/2
@@ -110,8 +147,22 @@ class printer:
         # Ud = Ub - Uy
         return Ux, Uy, Ua, Ub, Uc, Ud, 0
     
-    def executeCommand(self, in_command = []):
+    def closeSession(self):
+        if self.session is not None:
+            try:
+                self.session.close()
+            except Exception as e:
+                print(f"Error: {e}")
+            finally:
+                self.session = None
+        print("Session closed")
+        return 0
         
+    
+    def __del__(self):
+        self.closeSession()
+    
+    def executeCommand(self, in_command = []):
         
         if in_command != []:
             print(f"Executing command: {in_command}")
